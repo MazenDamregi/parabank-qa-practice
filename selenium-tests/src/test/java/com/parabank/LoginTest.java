9101e22f54d108f2ffb4d8e8015a9cdb97e40eb6
Mazen <damergimazen1234@gmail.com>
2026-03-07 22:34:06 +0100

    Add Selenium login automation test

---
+++ selenium-tests/src/test/java/com/parabank/LoginTest.java
@@ -0,0 +1,67 @@
+package com.parabank;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        // Selenium 4 manages ChromeDriver automatically - no setup needed
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void validLoginTest() {
+        // Go to parabank
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+
+        // Find username and password fields and login button
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@value='Log In']"));
+
+        // Enter credentials
+        username.sendKeys("john");
+        password.sendKeys("demo");
+        loginBtn.click();
+
+        // Verify we landed on the account page
+        Assert.assertTrue(driver.getTitle().contains("Accounts"), 
+            "Login failed - not on accounts page");
+    }
+
+    @Test
+    public void invalidLoginTest() {
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@value='Log In']"));
+
+        username.sendKeys("wronguser");
+        password.sendKeys("wrongpass");
+        loginBtn.click();
+
+        // Verify error message appears
+        WebElement error = driver.findElement(By.className("error"));
+        Assert.assertTrue(error.isDisplayed(), 
+            "Error message not shown for invalid login");
+    }
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
